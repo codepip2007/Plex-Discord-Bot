15f4426d612d491a0ac954fcf9058a3f92c892ee
codepip2007 <pepcplace@gmail.com>
2021-11-19 07:26:09 +0800

    various

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("webserver.js");
 const discord_js_1 = __importStar(require("discord.js"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
